fbf4e41ef45e62cf08e519e959ea02cc43e66048
LAIBAMEHMOOD <laibamehmood033@gmaile.com>
2024-03-02 11:25:04 +0500

    assigment no 3

---
+++ CASE.js
@@ -0,0 +1,4 @@
+var Name = "arsalan Imran";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
---
+++ CASE.ts
@@ -0,0 +1,7 @@
+let Name = "arsalan Imran";
+
+console.log(Name.toLowerCase());
+
+console.log(Name.toUpperCase());
+
+console.log(Name.replace(/\b\w/g, (char) => char.toUpperCase()));
